b07fdef7eef74309389b1a8e73fe3a7562f8a735
DavidTolaMamani <129429772+DavidTolaMamani@users.noreply.github.com>
2023-03-31 19:42:04 -0400

    Clase 2: Dinosaurio

--- dino_runner/components/game.py
+++ dino_runner/components/game.py
@@ -1,7 +1,7 @@
 import pygame
 import random
 from dino_runner.utils.constants import BG, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS,CLOUD
-
+from dino_runner.components.dinosaur import Dinosaur
 
 class Game:
     def __init__(self):
@@ -16,6 +16,7 @@ class Game:
         self.y_pos_bg = 380
         self.x_pos_cloud = 0
         self.y_pos_cloud = 0 
+        self.player = Dinosaur()
 
     def run(self):
         # Game loop: events - update - draw
@@ -31,14 +32,17 @@ class Game:
             if event.type == pygame.QUIT:
                 self.playing = False
 
+    
     def update(self):
-        pass
+        user_input = pygame.key.get_pressed()
+        self.player.update(user_input)
 
     def draw(self):
         self.clock.tick(FPS)
         self.screen.fill((255, 255, 255))
         self.draw_background()
         self.draw_cloud()
+        self.player.draw(self.screen)
         pygame.display.update()
         pygame.display.flip()
 
@@ -53,11 +57,11 @@ class Game:
     
     def draw_cloud(self):
         image_width = CLOUD.get_width()
-        self.screen.blit(CLOUD, (self.x_pos_cloud, self.y_pos_cloud))
         if self.x_pos_cloud + image_width < 0: 
-            self.x_pos_cloud = SCREEN_WIDTH + random.randint(900, 1000)
-            self.y_pos_cloud = random.randint(60, 120)
+            self.x_pos_cloud = SCREEN_WIDTH + random.randint(800, 1000)
+            self.y_pos_cloud = random.randint(60, 360)
         self.x_pos_cloud -= self.game_speed 
+        self.screen.blit(CLOUD, (self.x_pos_cloud, self.y_pos_cloud))
        
 
 
