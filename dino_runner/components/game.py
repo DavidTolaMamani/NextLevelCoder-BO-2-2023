aa3a26e7627993cc749c6fc967ea00cb11ef43a4
DavidTolaMamani <129429772+DavidTolaMamani@users.noreply.github.com>
2023-04-03 19:58:34 -0400

    clase 3: obstáculos añadidos

--- dino_runner/components/game.py
+++ dino_runner/components/game.py
@@ -2,6 +2,7 @@ import pygame
 import random
 from dino_runner.utils.constants import BG, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS,CLOUD
 from dino_runner.components.dinosaur import Dinosaur
+from dino_runner.components.obstacles.obstacle_manager import ObstacleManager
 
 class Game:
     def __init__(self):
@@ -17,6 +18,7 @@ class Game:
         self.x_pos_cloud = 0
         self.y_pos_cloud = 0 
         self.player = Dinosaur()
+        self.obstacle_manager = ObstacleManager()
 
     def run(self):
         # Game loop: events - update - draw
@@ -36,6 +38,7 @@ class Game:
     def update(self):
         user_input = pygame.key.get_pressed()
         self.player.update(user_input)
+        self.obstacle_manager.update(self)
 
     def draw(self):
         self.clock.tick(FPS)
@@ -43,6 +46,7 @@ class Game:
         self.draw_background()
         self.draw_cloud()
         self.player.draw(self.screen)
+        self.obstacle_manager.draw(self.screen)
         pygame.display.update()
         pygame.display.flip()
 
